bbcdd9b8dc947132e11a6e85a1858722a9b65db4
Dalton Neely <daltonjn1@gmail.com>
2020-05-08 18:39:29 -0600

    feat: Add Blockchain

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ class Transaction {
 }
 
 class Block {
-    constructor() {
+    constructor(previousHash) {
         this.transactions = []
         this.hash = ""
+        this.nonce = 0
+        this.previousHash = previousHash
     }
 
     static reduce = (finalString, transaction) => [
@@ -24,27 +26,76 @@ class Block {
         transaction.timestamp.toString()
     ].join()
 
+    static genesisBlock(target, hashFunction, creator) {
+        let block = new Block(null)
+        block.add(new Transaction("0", creator, 1000000))
+        block.hash = target
+        return block
+    }
+
     convertToString() {
         return this.transactions.reduce(Block.reduce)
     }
 
-    generateHash(hashFunction) {
-        this.hash = hashFunction.hex(this.convertToString())
+    generateHash(hashFunction, target) {
+        while (!this.hash.startsWith(target)) {
+            this.nonce++
+            this.hash = hashFunction.hex(this.convertToString() + this.nonce)
+        }
     }
 
     add(transaction) {
         this.transactions.push(transaction)
     }
 
-
 }
 
+class BlockChain {
+    constructor(target, hashFunction, creator) {
+        this.target = target
+        this.hasFunction = hashFunction
+        this.blocks = [Block.genesisBlock(target, hashFunction, creator)]
+        this.pending = []
+    }
 
-const transactionOne = new Transaction("foo", "bar", 1000.00)
-const transactionTwo = new Transaction("bar", "foo", 1000.00)
-const block = new Block()
-block.add(transactionOne)
-block.add(transactionTwo)
-block.generateHash(sha256)
+    transaction(transaction) {
+        this.pending.push(transaction)
+    }
 
-console.log(block)
+    mine() {
+        let block = new Block(this.target, this.blocks[this.blocks.length - 1].hash)
+        this.pending.forEach(transaction => block.add(transaction))
+        block.generateHash(this.hasFunction, this.target)
+        this.blocks.push(block)
+        this.pending = []
+    }
+
+    balance(id) {
+        let sum = 0
+        this.blocks.forEach(block => {
+            block.transactions.forEach(transaction => {
+                if (transaction.to === id) {
+                    sum += transaction.data
+                }
+                if (transaction.from === id){
+                    sum -= transaction.data
+                }
+            })
+        })
+        return sum
+    }
+}
+
+const blockChain = new BlockChain("0000", sha256, "dalton")
+
+const transactionOne = new Transaction("dalton", "taylor", 1000)
+const transactionTwo = new Transaction("taylor", "dalton", 500)
+
+blockChain.transaction(transactionOne)
+blockChain.transaction(transactionTwo)
+blockChain.mine()
+
+console.log(blockChain.balance("dalton"))
+blockChain.transaction(new Transaction("dalton", "tom", 406))
+blockChain.mine()
+console.log(blockChain.balance("dalton"))
