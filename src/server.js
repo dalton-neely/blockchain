95c628476ee90894177a13bb6541d9573733fc34
Dalton Neely <daltonjn1@gmail.com>
2020-05-10 15:46:38 -0600

    feat: Serve Static Resources

--- src/server.js
+++ src/server.js
@@ -2,9 +2,6 @@ import express from 'express';
 
 const PORT = 80;
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Blockchain Server');
-});
+app.use(express.static('src/static'));
 
 app.listen(PORT, () => console.log(`Running Blockchain Server on port ${PORT}`));
